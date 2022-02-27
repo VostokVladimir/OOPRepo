b7d80044627963b34698f56f31809e0d6af78fea
VostokVladimir <vostokirk@mail.ru>
2022-02-27 13:02:29 +0300

    создан проект , создана сцена , написаны скрипты движения

---
+++ OOP_Project/Assets/Scripts/PlayerBall.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FirstModule
+{
+    
+    public class PlayerBall : MonoBehaviour
+    {   public float speed = 3.0f;
+        private Rigidbody _rigidbody;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+            _rigidbody = GetComponent<Rigidbody>();
+
+
+        }
+
+        protected void Move()
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+            Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+            _rigidbody.AddForce(movement * speed);
+        }
+
+
+    }
+}
